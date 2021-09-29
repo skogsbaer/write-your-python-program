f39871b4981c17196bb3256ac5af4ba461f55a6d
CodeSteak <codesteak@shellf.art>
2021-09-29 22:45:26 +0200

    add unit test for return traces ast transformer

---
+++ python/deps/untypy/test/util_test/test_return_traces.py
@@ -0,0 +1,33 @@
+import ast
+import unittest
+
+from untypy.util.return_traces import ReturnTraceManager, ReturnTracesTransformer
+
+
+class TestAstTransform(unittest.TestCase):
+
+    def test_ast_transform(self):
+        src = """
+def foo(flag: bool) -> int:
+    print('Hello World')
+    if flag:
+        return 1
+    else:
+        return 'you stupid'
+        """
+        target = """
+def foo(flag: bool) -> int:
+    print('Hello World')
+    if flag:
+        untypy._before_return(0)
+        return 1
+    else:
+        untypy._before_return(1)
+        return 'you stupid'
+        """
+
+        tree = ast.parse(src)
+        ReturnTracesTransformer("<dummyfile>", ReturnTraceManager()).visit(tree)
+        ast.fix_missing_locations(tree)
+        self.assertEqual(ast.unparse(tree).strip(), target.strip())
+
