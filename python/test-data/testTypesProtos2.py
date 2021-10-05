e8f256e76c15d0029d39035488f0667ef899a794
CodeSteak <codesteak@shellf.art>
2021-10-05 18:36:14 +0200

    make tests reproducible

--- python/test-data/testTypesProtos2.py
+++ python/test-data/testTypesProtos2.py
@@ -12,6 +12,8 @@ class Dog:
     # incorrect implementation of the Animal protocol
     def makeSound(self, loadness: int) -> str:
         return f"{loadness} wuffs"
+    def __repr__(self) -> str:
+        return f"<{type(self).__qualname__}>"
 
 def doSomething(a: Animal) -> None:
     print(a.makeSound(3.14))
