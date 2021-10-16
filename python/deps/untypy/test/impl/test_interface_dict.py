1779e319d0c2d9d783b3f8a81310cd5e73d5dd8d
CodeSteak <codesteak@shellf.art>
2021-10-16 17:57:52 +0200

    test blame assignment I

--- python/deps/untypy/test/impl/test_interface_dict.py
+++ python/deps/untypy/test/impl/test_interface_dict.py
@@ -1,7 +1,18 @@
 import unittest
 
 import untypy
-from untypy.error import UntypyTypeError
+from untypy.error import UntypyTypeError, Location
+
+
+def dummy_caller_2(ch, arg):
+    return ch(arg)
+
+
+# Sets up callstack so that this function is blamed for creation.
+# untypy.checker uses the function that called the function that
+# invokes the checker.
+def dummy_caller(ch, arg):
+    return dummy_caller_2(ch, arg)
 
 
 class TestInterfaceDict(unittest.TestCase):
@@ -10,45 +21,51 @@ class TestInterfaceDict(unittest.TestCase):
     """
 
     def setUp(self) -> None:
-        ch1 = untypy.checker(lambda: dict[int, str], TestInterfaceDict.setUp)
+        ch1 = untypy.checker(lambda: dict[int, str], dummy_caller)
 
         # A: No Errors
-        self.good = ch1({
+        self.good = dummy_caller(ch1, {
             1: "one",
             2: "two",
             3: "three",
         })
 
         # B: Value error on 2
-        self.valerr = ch1({
+        self.valerr = dummy_caller(ch1, {
             1: "one",
             2: 2,
             3: "three",
         })
 
         # B: Key error on 2
-        self.keyerr = ch1({
+        self.keyerr = dummy_caller(ch1, {
             1: "one",
             "two": 2,
             3: "three",
         })
+
+    def assertBlame(self, cm, caller):
+        self.assertTrue(cm.exception.last_responsable() in Location.from_code(caller))
 
     def test_get(self):
         self.assertEqual(self.good.get(1), "one")
         self.assertEqual(self.good.get(4), None)
         self.assertEqual(self.good.get(4, "four"), "four")
 
-        with self.assertRaises(UntypyTypeError):
+        with self.assertRaises(UntypyTypeError) as cm:
             # 42 must be str
             self.good.get(1, 42)
+        self.assertBlame(cm, TestInterfaceDict.test_get)
 
-        with self.assertRaises(UntypyTypeError):
+        with self.assertRaises(UntypyTypeError) as cm:
             # key must be int
             self.good.get("two")
+        self.assertBlame(cm, TestInterfaceDict.test_get)
 
-        with self.assertRaises(UntypyTypeError):
+        with self.assertRaises(UntypyTypeError) as cm:
             # value err
             self.valerr.get(2)
+        self.assertBlame(cm, dummy_caller)
 
     def test_items(self):
         self.assertEqual(
@@ -56,10 +73,10 @@ class TestInterfaceDict(unittest.TestCase):
             [(1, "one"), (2, "two"), (3, "three")]
         )
 
-        with self.assertRaises(UntypyTypeError):
+        with self.assertRaises(UntypyTypeError) as cm:
             list(self.keyerr.items())
 
-        with self.assertRaises(UntypyTypeError):
+        with self.assertRaises(UntypyTypeError) as cm:
             list(self.valerr.items())
 
     def test_keys(self):
