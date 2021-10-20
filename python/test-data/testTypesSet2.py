5238fcde46c0ab41480119083ed6b8d3e4bb406c
Stefan Wehr <stefan.wehr@hs-offenburg.de>
2021-10-20 18:13:33 +0200

    make tests deterministic

--- python/test-data/testTypesSet2.py
+++ python/test-data/testTypesSet2.py
@@ -6,5 +6,5 @@ def foo(l: set[Callable[[], str]]) -> list[str]:
         res.append(f())
     return res
 
-print(foo(set([lambda: "1", lambda: "2"])))
+print(sorted(list(foo(set([lambda: "1", lambda: "2"])))))
 foo(set([lambda: "1", lambda: 42])) # error because the 2nd functions returns an int
