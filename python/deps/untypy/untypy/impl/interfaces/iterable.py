ce7cf2ac682cb3bb785eaff2015f1c041e34d9d1
Stefan Wehr <stefan.wehr@hs-offenburg.de>
2022-05-04 16:02:28 +0200

    add note

--- python/deps/untypy/untypy/impl/interfaces/iterable.py
+++ python/deps/untypy/untypy/impl/interfaces/iterable.py
@@ -4,6 +4,8 @@ import typing
 
 I = TypeVar("I")
 
+# Note: using typing.Iterator as the result creates an indirection that avoids an infinite
+# loop when constructing checkers.
 class Iterable(Generic[I]):
     def __iter__(self) -> typing.Iterator[I]:
         pass
