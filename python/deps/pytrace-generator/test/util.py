c2030eeeb0c5e81ef73c750e3be3abf80f3447d1
Hannes Braun <hannes@hannesbraun.net>
2024-10-09 22:45:16 +0200

    Try to normalize references to heap values in tests

--- python/deps/pytrace-generator/test/util.py
+++ python/deps/pytrace-generator/test/util.py
@@ -11,6 +11,28 @@ def read_trace_entries(data):
         if idx >= 0:
             # Remove user specific path stuff to allow for equality testing
             entry["filePath"] = entry["filePath"][idx + len(test_cases_str):]
+        normalize_refs(entry)
         entries.append(entry)
         data = data[entry_len:]
     return entries
+
+def normalize_refs(entry):
+    next_id = 0
+    mapping = {}
+    for frame in entry["stack"]:
+        for var in frame["locals"]:
+            if var["type"] == "ref":
+                original_id = var["value"]
+                if original_id in mapping:
+                    var["value"] = mapping[original_id]
+                else:
+                    var["value"] = next_id
+                    mapping[original_id] = next_id
+                    if str(next_id) in entry["heap"]:
+                        tmp = entry["heap"][str(next_id)]
+                        entry["heap"][str(next_id)] = entry["heap"][str(original_id)]
+                        entry["heap"][str(original_id)] = tmp
+                    else:
+                        entry["heap"][str(next_id)] = entry["heap"][str(original_id)]
+                        del entry["heap"][str(original_id)]
+                    next_id += 1
