eafeefe130ef9f2b78d12902c836ea2ec22028e0
Hannes Braun <hannes@hannesbraun.net>
2024-10-24 14:38:30 +0200

    Change visualization of return value

--- src/programflow-visualization/frontend/HTMLGenerator.ts
+++ src/programflow-visualization/frontend/HTMLGenerator.ts
@@ -167,11 +167,10 @@ export class HTMLGenerator {
 
     private frameSubItem(frameName: string, namedValue: NamedValue): string {
         const isReturn = namedValue.name === 'return';
-        const displayName = isReturn ? 'Return value' : namedValue.name;
         return `
             <div class="row frame-item" id="subItem${toID(namedValue.name)}">
                 <div class="name-border${isReturn ? ' return-value' : ''}">
-                    ${escapeHTML(displayName)}
+                    ${escapeHTML(namedValue.name)}
                 </div>
                 <div class="value-border" ${namedValue.type === 'ref' ? `id="${toID(frameName)}${toID(namedValue.name)}Pointer${toID(namedValue.value)}"` : ''}>
                     ${this.getCorrectValueOf(namedValue)}
